59734eb01dbbbe439fd1638c6217a9360978a652
Beyza Hilal <beyzakaramert@gmail.com>
2024-07-03 11:32:39 +0300

    [Update]

--- MiniMaxSum/Program.cs
+++ MiniMaxSum/Program.cs
@@ -1,7 +1,54 @@
-﻿namespace MiniMaxSum
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
 {
-    public class Program
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+
+        long minSum = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            minSum += arr[i];
+        }
+
+        long maxSum = 0;
+        for (int i = 1; i < 5; i++)
+        {
+            maxSum += arr[i];
+        }
+
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
     {
 
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
     }
 }
